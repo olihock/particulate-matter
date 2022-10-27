4778157f567fc5202c6abec2aed4ddfe84ca4637
Oliver Hock <oliver.hock@mecom.de>
2022-10-27 14:01:00 +0200

    change linear regression

--- de/ithoc/particulatematter/linear-regression-sensor.py
+++ de/ithoc/particulatematter/linear-regression-sensor.py
@@ -10,10 +10,6 @@ import glob
 
 print("TensorFlow version: ", tf.__version__)
 
-#
-# Data Set
-#
-
 # Read the sensor and wind data from file and create a csv handler
 path = os.getcwd()
 data_file = glob.glob(os.path.join(path, "2022-09_sensor-wind-p1.csv"))
@@ -29,7 +25,7 @@ print(dataset.tail())
 
 training_set = dataset.sample(frac=0.7, random_state=0)
 print("train_set.size: ", training_set.__len__())
-test_set = dataset.drop(training_set.index)
+test_set = dataset.drop(training_set.index)  # for later use
 print("test_set.size: ", test_set.__len__())
 
 # Split features and labels for both the training and test set.
@@ -39,28 +35,28 @@ training_labels = training_features.pop('P1')
 test_features = test_set.copy()
 test_labels = test_features.pop('P1')
 
-#
 # Univariate Linear Regression (Daytime)
-#
-
 daytime_features = np.array(training_features['Daytime'])
 print('daytime_features:    ', daytime_features)
 
-# Input shape of this normalisation is a tensor with dimension 1xn
+# Normalise input data if needed.
 daytime_normalisation = layers.Normalization(input_shape=[1, ], axis=None)
 daytime_normalisation.adapt(daytime_features)
 
+# Build the ML model by using previously created layers.
 daytime_model = tf.keras.Sequential([
     daytime_normalisation,
     layers.Dense(units=1)
 ])
 daytime_model.summary()
 
+# Define cost function algorithm and put everything together.
 daytime_model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
     loss='mean_absolute_error'
 )
 
+# Learn parameters for the training examples.
 daytime_fit = daytime_model.fit(
     training_features['Daytime'],
     training_labels,
@@ -69,6 +65,7 @@ daytime_fit = daytime_model.fit(
     validation_split=0.2
 )
 
+# Predict new examples by putting in times of day.
 x = tf.linspace(0, 1, 100)
 y = daytime_model.predict(x)
 print('y: ', y)
